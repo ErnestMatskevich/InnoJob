45311170205c4815c492acfdd1a2ea9902ce45c1
Ernest <58702017+ErnestMatskevich@users.noreply.github.com>
2025-03-04 17:29:40 +0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,32 @@
+import logging
+from aiogram import Bot, Dispatcher, types
+from aiogram.enums import ParseMode
+from aiogram.filters import Command
+from aiogram.types import Message
+
+
+API_TOKEN = '5815079756:AAHtCSc9jtcKd9Wl7zPOVnEi_ev0qV3Y87c'
+
+logging.basicConfig(level=logging.INFO)
+
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher()
+
+
+@dp.message(Command("start"))
+async def cmd_start(message: Message):
+    await message.answer("Start")
+
+
+@dp.message(Command("help"))
+async def cmd_help(message: Message):
+    await message.answer("Вы обратились к справке бота")
+
+
+async def main():
+    await dp.start_polling(bot)
+
+
+if __name__ == '__main__':
+    import asyncio
+    asyncio.run(main())
